39fe9fd91c20a80489465888d29c2752a7f791eb
Tobey <gronadebob@gmail.com>
2024-11-01 15:55:08 +1000

    Added credits and credits menu

---
+++ Scripts/UI/Credits.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pages;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private KeyboardInput keyboardInput;
+    [SerializeField] private GameObject closedControls;
+    [SerializeField] private GameObject openControls;
+
+    private int page;
+
+    void Update()
+    {
+        // Only open credits when not in Keyboard Input menu
+        if (!keyboardInput.InMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                canvas.enabled = !canvas.enabled;
+            }
+        }
+        else
+        {
+            canvas.enabled = false;
+        }
+
+        if (canvas.enabled)
+        {
+            // Show correct controls
+            closedControls.SetActive(false);
+            openControls.SetActive(true);
+
+            // Navigation
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                page--;
+            }
+            else if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                page++;
+            }
+
+            // Wrap navigation
+            if (page < 0)
+            {
+                page = pages.Length - 1;
+            }
+            else if (page >= pages.Length)
+            {
+                page = 0;
+            }
+
+            // Show current page
+            for (int i = 0; i < pages.Length; i++)
+            {
+                pages[i].SetActive(i == page);
+            }
+        }
+        else
+        {
+            // Show correct controls
+            closedControls.SetActive(true);
+            openControls.SetActive(false);
+
+            page = 0;
+        }
+    }
+}
